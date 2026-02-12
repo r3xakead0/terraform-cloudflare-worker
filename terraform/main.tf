f4759afe18457603019066adbe5d3a618452619a
Afu Tse <solapa@gmail.com>
2026-02-12 14:47:39 -0500

    Add Cloudflare workers subdomain resources and update outputs

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,3 +3,9 @@ resource "cloudflare_workers_script" "node_app" {
   account_id = var.account_id
   content    = file("../app/worker.js")
 }
+
+resource "cloudflare_workers_script_subdomain" "workers_dev" {
+  account_id = var.account_id
+  script_name = cloudflare_workers_script.node_app.name
+  enabled = true
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,7 @@ output "worker_name" {
 output "worker_id" {
   value = cloudflare_workers_script.node_app.id
 }
+
+output "worker_url" {
+  value = "https://${var.worker_name}.${data.cloudflare_account.this.workers_subdomain}.workers.dev"
+}
